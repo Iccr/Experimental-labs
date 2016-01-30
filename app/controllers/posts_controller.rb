a3ecf494c45fdfc76079b55d73b5db97c0f41613
ccr <sis.ccr@gmail.com>
2016-01-30 11:30:21 +0545

    crud operation added in posts contoller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,53 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    binding.pry
+    post = Post.find(params[:id])
+    if post.save(post_params)
+      flash[:msg]= "post created"
+    else
+      flash[:msg]= "post failed"
+    end
+    redirect_to root_path
   end
 
   def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      flash[:msg]= "post created"
+    else
+      flash[:msg]= "post failed"
+    end
+    redirect_to root_path
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    post = Post.find(params[:id])
+    if post.destroy(post_params)
+      flash[:msg]= "post deleted"
+    else
+      flash[:msg]= "cant delete this post"
+    end
+    redirect_to root_path
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
