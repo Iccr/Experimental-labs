e857714e473704fe2bd432385db9d7106fc201de
ccr <sis.ccr@gmail.com>
2016-01-30 20:29:56 +0545

    migration to change the table type to text was generated

---
+++ db/migrate/20160130144410_change_table_column_type.rb
@@ -0,0 +1,5 @@
+class ChangeTableColumnType < ActiveRecord::Migration
+  def change
+    change_column :posts, :description, :text, :limit => nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130052821) do
+ActiveRecord::Schema.define(version: 20160130144410) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
-    t.string   "description", limit: 255
+    t.text     "description", limit: 65535
     t.integer  "user_id",     limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
