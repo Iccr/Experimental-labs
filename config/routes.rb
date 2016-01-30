b2dd426848e99510b62cf98655f28704106b8e1a
ccr <sis.ccr@gmail.com>
2016-01-30 13:06:53 +0545

    root path for authenticated and unauthenticated users added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,15 @@ resources :posts
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
 
+  devise_scope :user do
+      authenticated :user do
+        root :to => 'posts#index'
+      end
+      unauthenticated :user do
+        root :to => 'home#index', as: :unauthenticated_root
+      end
+    end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
